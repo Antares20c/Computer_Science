daabf67e0ba3669530c0972f6432c8c702c95533
Antares20c <antares20c@gmail.com>
2023-06-17 01:38:23 -0600

    Pilas con JS

---
+++ Pilas.js
@@ -0,0 +1,141 @@
+// Parte 1
+class Pila {
+    constructor() {
+        this.elementos = [];
+    }
+
+    push(elemento) {
+        this.elementos.push(elemento);
+    }
+
+    pop() {
+        if (this.elementos.length === 0) {
+            return "La pila está vacía";
+        }
+        return this.elementos.pop();
+    }
+
+    peek() {
+        if (this.elementos.length === 0) {
+            return "La pila está vacía";
+        }
+        return this.elementos[this.elementos.length - 1];
+    }
+
+    isEmpty() {
+        return this.elementos.length === 0;
+    }
+
+    size() {
+        return this.elementos.length;
+    }
+
+    clear() {
+        this.elementos = [];
+    }
+}
+
+function obtenerElementos(pila, numero) {
+    const elementosObtenidos = [];
+
+    while (numero > 0 && !pila.isEmpty()) {
+        elementosObtenidos.push(pila.pop())
+        numero--;
+    }
+
+    return elementosObtenidos.reverse();
+}
+
+const pila = new Pila();
+pila.push("Manzana");
+pila.push("Cebolla");
+pila.push("Apio");
+pila.push("Naranja");
+pila.push("Papaya");
+pila.push("Sandia");
+pila.push("Mélon");
+
+const elementos = obtenerElementos(pila, 4);
+console.log(elementos);
+
+
+
+// Parte 2
+class Pila1 {
+  constructor() {
+    this.elementos = [];
+  }
+
+  push(elemento) {
+    this.elementos.push(elemento);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return "La pila está vacía";
+    }
+    return this.elementos.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return "La pila está vacía";
+    }
+    return this.elementos[this.elementos.length - 1];
+  }
+
+  isEmpty() {
+    return this.elementos.length === 0;
+  }
+
+  size() {
+    return this.elementos.length;
+  }
+
+  clear() {
+    this.elementos = [];
+  }
+}
+
+function reemplazar(pila, nuevo, viejo) {
+  const pilaTemp = new Pila1();
+  let encontrado = false;
+
+  while (!pila.isEmpty()) {
+    const elemento = pila.pop();
+    if (elemento === viejo && !encontrado) {
+      pilaTemp.push(nuevo);
+      encontrado = true;
+    } else {
+      pilaTemp.push(elemento);
+    }
+  }
+
+  const nuevaPila = new Pila1();
+  while (!pilaTemp.isEmpty()) {
+    nuevaPila.push(pilaTemp.pop());
+  }
+
+  return nuevaPila;
+}
+
+const pila1 = new Pila1();
+pila1.push(3);
+pila1.push(2);
+pila1.push(3);
+pila1.push(4);
+pila1.push(6);
+pila1.push(8);
+pila1.push(1);
+pila1.push(2);
+pila1.push(5);
+pila1.push(5);
+
+const nuevaPila = reemplazar(pila1, 7, 2);
+
+const salida = [];
+while (!nuevaPila.isEmpty()) {
+  salida.unshift(nuevaPila.pop());
+}
+
+console.log("Salida:", salida);
---
+++ Pilas1.js
@@ -0,0 +1,55 @@
+// Pilas 3
+const pueblos = ["Pueblo Origen", "Pueblo1", "Pueblo2", "Pueblo3", "Destino"];
+
+const pilaIda = [];
+const pilaVuelta = [];
+
+console.log("Recorrido:", pueblos);
+for (let i = 0; i < pueblos.length; i++) {
+    const pueblo = pueblos[i];
+    pilaIda.push(pueblo);
+}
+
+while (pilaIda.length > 0) {
+    const pueblo = pilaIda.pop();
+    pilaVuelta.push(pueblo);
+}
+
+console.log("Regreso:", pilaVuelta)
+
+// Pilas 4
+function retirarContenedor(pila, contenedor) {
+    const pilaAuxiliar = [];
+    let contenedorEncontrado = false;
+  
+    while (!contenedorEncontrado && pila.length > 0) {
+      const contenedorActual = pila.pop();
+      if (contenedorActual === contenedor) {
+        contenedorEncontrado = true;
+      } else {
+        pilaAuxiliar.push(contenedorActual);
+      }
+    }
+  
+    while (pilaAuxiliar.length > 0) {
+      pila.push(pilaAuxiliar.pop());
+    }
+  
+    if (contenedorEncontrado) {
+      console.log(`El contenedor ${contenedor} ha sido retirado.`);
+    } else {
+      console.log(`El contenedor ${contenedor} no se encuentra en la pila.`);
+    }
+  
+    return pila;
+}
+
+const Contenedores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const contenedorRetirado = 5;
+
+console.log("Pila Original:", Contenedores);
+console.log("Contenedor a retirar:", contenedorRetirado);
+
+const pilaActualizada = retirarContenedor(Contenedores, contenedorRetirado);
+
+console.log("Pila Actualizada:", pilaActualizada)
